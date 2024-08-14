96b7f7e4e32270dc7ef98fac72fcf29c13478888
HyungminYoon1 <156720686+HyungminYoon1@users.noreply.github.com>
2024-08-14 17:26:16 +0900

    feat: insert 로직 추가

--- src/main/java/com/yojulab/study_springboot/controller/HRController.java
+++ src/main/java/com/yojulab/study_springboot/controller/HRController.java
@@ -1,41 +1,43 @@
-package com.yojulab.study_springboot.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
-
-@Controller
-@RequestMapping("/hrproject")
-public class HRController {
-
-    @GetMapping("/home")
-    public ModelAndView home(ModelAndView modelAndView){
-        modelAndView.setViewName("/WEB-INF/views/hr/main.jsp");
-        return modelAndView;
-    }
-    @GetMapping("/delete")
-    public ModelAndView delete(ModelAndView modelAndView){
-        modelAndView.setViewName("/WEB-INF/views/hr/delete.jsp");
-        return modelAndView;
-    }
-
-    @GetMapping("/insert")
-    public ModelAndView insert(ModelAndView modelAndView){
-        modelAndView.setViewName("/WEB-INF/views/hr/insert.jsp");
-        return modelAndView;
-    }
-
-    @GetMapping("/select")
-    public ModelAndView select(ModelAndView modelAndView){
-        modelAndView.setViewName("/WEB-INF/views/hr/select.jsp");
-        return modelAndView;
-    }
-
-    @GetMapping("/dept")
-    public ModelAndView selectDept(ModelAndView modelAndView) {
-        modelAndView.setViewName("/WEB-INF/views/hr/select-dept.jsp");
-        return modelAndView;
-    }
-}
+//package com.yojulab.study_springboot.controller;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.servlet.ModelAndView;
+//
+//@Controller
+//@RequestMapping("/hrproject")
+//public class HRController {
+//
+//    @GetMapping("/home")
+//    public ModelAndView home(ModelAndView modelAndView){
+//        modelAndView.setViewName("/WEB-INF/views/hr/main.jsp");
+//        return modelAndView;
+//    }
+//    @GetMapping("/delete")
+//    public ModelAndView delete(ModelAndView modelAndView){
+//        modelAndView.setViewName("/WEB-INF/views/hr/delete.jsp");
+//        return modelAndView;
+//    }
+//
+//    @GetMapping("/insert")
+//    public ModelAndView insert(ModelAndView modelAndView){
+//        modelAndView.setViewName("/WEB-INF/views/hr/insert.jsp");
+//        return modelAndView;
+//    }
+//
+//    @GetMapping("/select")
+//    public ModelAndView select(ModelAndView modelAndView){
+//        modelAndView.setViewName("/WEB-INF/views/hr/select.jsp");
+//        return modelAndView;
+//    }
+//
+//    @GetMapping("/dept")
+//    public ModelAndView selectDept(ModelAndView modelAndView) {
+//        modelAndView.setViewName("/WEB-INF/views/hr/select-dept.jsp");
+//        return modelAndView;
+//    }
+//
+//
+//}
